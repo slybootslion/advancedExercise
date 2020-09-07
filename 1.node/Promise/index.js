3b1ade3740e44645c9f29c2b2ada4fcee74b1806
slybootslion <slybootslion@vip.qq.com>
2020-09-07 22:38:14 +0800

    2020-09-07 22:38:13

--- 1.node/Promise/index.js
+++ 1.node/Promise/index.js
@@ -1,8 +1,13 @@
-const BasicPromise = require('./basic-promise.js')
-const bp = new BasicPromise((resolve, reject) => {
+const BasicPromise = require('./BasicPromise.js')
+const MyPromise = require('./Promise_A+.js')
+const bp = new MyPromise((resolve, reject) => {
   // throw new Error('err msg')
   // resolve('成功的内容')
   // reject('失败的内容')
+
+  setTimeout(() => {
+    resolve('成功的回调，定时器里')
+  }, 1000)
 })
 
 bp.then((data) => {
