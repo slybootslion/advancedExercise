4ab314c486eed01d86501443a8ba806c8b5b0d6b
slybootslion <slybootslion@vip.qq.com>
2020-11-20 10:06:03 +0800

    2020-11-20 10:06:03

---
+++ 1_node/7.koa-analyse/koa-analyse/index-koa.js
@@ -0,0 +1,62 @@
+const Koa = require('./koa')
+const fs = require('fs')
+const path = require('path')
+
+const app = new Koa()
+
+// 基础实现
+/* app.use(function (ctx) {
+  // console.log(ctx.req.url)
+  // console.log(ctx.request.req.url)
+  // console.log(ctx.request.path)
+  // console.log(ctx.path)
+  // ctx.body = 'hello world'
+  // 文本
+  // ctx.body = 'hello koa'
+  // 文件
+  // ctx.body = fs.createReadStream(path.join(__dirname, '../testfile/index.html'))
+  // json
+  ctx.body = { body: 'json' } 
+  // console.log(ctx.body)
+}) */
+
+function sleep() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+      console.log('sleep')
+    }, 1000)
+  })
+}
+// 中间件
+app.use(async (ctx, next) => {
+  console.log(1)
+  await next()
+  // throw new Error('handle throw error')
+  console.log(2)
+})
+
+app.use(async (ctx, next) => {
+  console.log(3)
+  await sleep()
+  await next()
+  console.log(4)
+})
+
+app.use(async (ctx, next) => {
+  console.log(5)
+  await next()
+  console.log(6)
+  ctx.body = 'koa analyse'
+})
+
+// 错误处理
+app.on('error', (params) => {
+  console.log('*******')
+  console.log(params)
+  console.log('*******')
+})
+
+app.listen(10000, () => {
+  console.log('监听10000端口')
+})
--- 1_node/7.koa-analyse/koa-analyse/koa/lib/application.js
+++ 1_node/7.koa-analyse/koa-analyse/koa/lib/application.js
@@ -1,19 +1,23 @@
 const http = require('http')
 const Stream = require('stream')
+const EventEmitter = require('events')
 
 const context = require('./context.js')
 const request = require('./request.js')
 const response = require('./response.js')
 
-class Applications {
+class Applications extends EventEmitter {
   constructor() {
+    super()
     this.context = Object.create(context)
     this.request = Object.create(request)
     this.response = Object.create(response)
+    this.middlewares = []
   }
 
   use(fn) {
-    this.fn = fn
+    this.middlewares.push(fn)
+    // this.fn = fn
   }
 
   createContext(req, res) {
@@ -27,21 +31,45 @@ class Applications {
     return ctx
   }
 
+  async compose(ctx) {
+    let index = -1
+    // 异步迭代用函数 同步迭代用循环
+    const dispatch = (idx) => {
+      if (idx <= index) return Promise.reject(new Error('next() called multiple times'))
+      index = idx
+      if (idx === this.middlewares.length) return Promise.resolve()
+      const middleware = this.middlewares[idx]
+
+      try {
+        return Promise.resolve(middleware(ctx, () => dispatch(++idx)))
+      } catch (err) {
+        return Promise.reject(err)
+      }
+    }
+    return dispatch(0)
+  }
+
   handleRequest(req, res) {
     res.statusCode = 404
     const ctx = this.createContext(req, res)
-    this.fn(ctx)
+    // this.fn(ctx)
+    this.compose(ctx).then(() => {
+      const body = ctx.body
+      if (typeof body === 'string' || Buffer.isBuffer(body)) {
+        res.end(body)
+      } else if (body instanceof Stream) {
+        body.pipe(res)
+      } else if (typeof body === 'object' && body != null) {
+        res.end(JSON.stringify(body))
+      } else {
+        res.end('Not Found')
+      }
+    }).catch(err => this.emit('error', err))
 
-    const body = ctx.body
-    if (typeof body === 'string' || Buffer.isBuffer(body)) {
-      res.end(ctx.body)
-    } else if (body instanceof Stream) {
-      body.pipe(res)
-    } else if (typeof body === 'object' && body != null) { 
-      res.end(JSON.stringify(body))
-    }else {
-      res.end('Not Found')
-    }
+    this.on('error', () => {
+      res.statusCode = 500
+      res.end('Internal Server Error')
+    })
   }
 
   listen(...args) {
